bf94e653c568016526e65e6b65e9c2ad7e51f958
Manish <mani.2280@gmail.com>
2025-07-16 20:56:59 +0530

    disable pop up

--- Framework/TestFramework/src/test/java/com/tests/BaseTest.java
+++ Framework/TestFramework/src/test/java/com/tests/BaseTest.java
@@ -1,9 +1,13 @@
 package com.tests;
 
+import java.io.File;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,7 +16,14 @@ public class BaseTest {
 	
 	@BeforeMethod
 	public void init() {
-		driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		// https://peter.sh/experiments/chromium-command-line-switches/#password-store
+		chromeOptions.addArguments("--start-maximized");
+		chromeOptions.addArguments("--disable-popup-blocking");
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", System.getProperty("user.dir") + File.separator + "Downloads");
+		chromeOptions.setExperimentalOption("prefs", prefs);
+		driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
